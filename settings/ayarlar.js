2d9577f24cf91a80389bfeb171c0cc46d80ec3a8
Midori <115791608+Slytherix@users.noreply.github.com>
2023-04-14 22:33:27 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "MTA0MTY1MTg1OTU5ODg3MjYwOA.GJJbFt.usHTCWc3KgIdxCRw3NJ40lS2tQiJqFS_0AIUOA", 
+    TOKEN: process.env.TOKEN || "none", 
     PREFIX: process.env.PREFIX || "+" , 
-    SAHİP: process.env.SAHİP || "406048010057809920", 
+    SAHİP: process.env.SAHİP || "none", 
 }
